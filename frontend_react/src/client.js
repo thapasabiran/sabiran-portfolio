cb33f7c5c88c668af7393200403869f154921a43
sabiranthapa <nexusvaultsystems@gmail.com>
2025-11-29 01:09:58 -0500

    no token

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -6,7 +6,6 @@ export const client = createClient({
   dataset: 'production',
   apiVersion: '2024-01-08',
   useCdn: true,
-  token: 'skUiR1PIxzQO1UuXXR5eXdZloVhbPKHU0qE03rMqPx6ceJz30XW0uO1EbKLlbOeA4i6neY378oTxkPhaWxP0oWovkoYd4stcYdusV4qdyOEQpk7HH7ekL4XRAObSnGSljqardjzLLUb0AdrvHEUE8pEmwpC3D1RtAbT3Dem0U2kM3nNJ4I9r', //process.env.SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
